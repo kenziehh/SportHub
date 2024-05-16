7065c0dc513649e2987f8c611304929706f8fd3c
kenziehh <ghaisani.nurani@gmail.com>
2024-05-16 22:45:37 +0700

    update news table and fix warning

--- resources/js/Pages/Admin/News/Index.tsx
+++ resources/js/Pages/Admin/News/Index.tsx
@@ -24,71 +24,85 @@ const NewsDashboard = ({ auth, news }: PageProps) => {
                     </SecondaryButton>
                 </section>
                 <section className="overflow-x-auto">
-                    <table className="">
-                        <th className="text-center px-2 py-3 border-2">
-                            Title
-                        </th>
-                        <th className="text-center px-2 py-3 border-2">
-                            Content
-                        </th>
-                        <th className="text-center px-2 py-3 border-2">
-                            Image
-                        </th>
-                        <th className="text-center px-2 py-3 border-2">
-                            Author
-                        </th>
-                        <th className="text-center px-2 py-3 border-2">
-                            Action
-                        </th>
-                        {news.data.map((newsData: NewsResponse) => {
-                            return (
-                                <tr key={newsData.id}>
-                                    <td className="px-2 py-3 border-2 max-w-sm md:text-justify">
-                                        {newsData.title}
-                                    </td>
-                                    <td className="px-2 py-3 border-2 max-w-6xl text-justify">
-                                        {newsData.content}
-                                    </td>
-                                    <td className="px-2 py-3 border-2">
-                                        <img
-                                            src={
-                                                newsData.image_url.startsWith(
-                                                    "news_images"
-                                                )
-                                                    ? `/storage/${newsData.image_url}`
-                                                    : newsData.image_url
-                                            }
-                                            alt=""
-                                            className="max-w-48 max-h-48 md:max-w-96 md:max-h-96"
-                                        />
-                                    </td>
-                                    <td className="px-2 py-3 border-2 text-justify">
-                                        {newsData.author}
-                                    </td>
-                                    <td className="px-2 py-3 border-2 text-justify">
-                                        <div className="inline-flex flex-col gap-2">
-                                            <PrimaryButton>
-                                                <Link
-                                                    href={route(
-                                                        "news.edit",
-                                                        newsData.id
-                                                    )}
-                                                >
-                                                    Edit
-                                                </Link>
-                                            </PrimaryButton>
-                                            <DangerButton
-                                                onClick={(e) =>
-                                                    deleteNews(newsData)
+                    <table className="w-full">
+                        <thead>
+                            <tr>
+                                <th className="text-center px-2 py-3 border-2">
+                                    Title
+                                </th>
+                                <th className="text-center px-2 py-3 border-2">
+                                    Content
+                                </th>
+                                <th className="text-center px-2 py-3 border-2">
+                                    Image
+                                </th>
+                                <th className="text-center px-2 py-3 border-2">
+                                    Category
+                                </th>
+                                <th className="text-center px-2 py-3 border-2">
+                                    Author
+                                </th>
+                                <th className="text-center px-2 py-3 border-2">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {news.data.map((newsData: NewsResponse) => {
+                                return (
+                                    <tr key={newsData.id}>
+                                        <td className="px-2 py-3 border-2 max-w-sm md:text-justify">
+                                            {newsData.title}
+                                        </td>
+                                        <td className="px-2 py-3 border-2 max-w-6xl text-justify">
+                                            {newsData.content}
+                                        </td>
+                                        <td className="px-2 py-3 border-2">
+                                            <img
+                                                src={
+                                                    newsData.image_url
+                                                        ? newsData.image_url.startsWith(
+                                                              "news_images"
+                                                          )
+                                                            ? `/storage/${newsData.image_url}`
+                                                            : newsData.image_url
+                                                        : ""
                                                 }
-                                            >
-                                                Delete
-                                            </DangerButton>
-                                        </div>
-                                    </td>
-                                </tr>
-                            );
-                        })}
+                                                alt="no picture"
+                                                className="max-w-48 max-h-48 md:max-w-96 md:max-h-96"
+                                            />
+                                        </td>
+                                        <td className="px-2 py-3 border-2 text-justify">
+                                            {newsData.category}
+                                        </td>
+                                        <td className="px-2 py-3 border-2 text-justify">
+                                            {newsData.author}
+                                        </td>
+                                        <td className="px-2 py-3 border-2 text-justify">
+                                            <div className="inline-flex flex-col gap-2">
+                                                <PrimaryButton>
+                                                    <Link
+                                                        href={route(
+                                                            "news.edit",
+                                                            newsData.id
+                                                        )}
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                </PrimaryButton>
+                                                <DangerButton
+                                                    onClick={(e) =>
+                                                        deleteNews(newsData)
+                                                    }
+                                                >
+                                                    Delete
+                                                </DangerButton>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
                     </table>
                 </section>
                 <Pagination links={news.meta.links} />
