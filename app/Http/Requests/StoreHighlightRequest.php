d1d5ddd32fccdc8c6b7cde0b15eb03e94a0002d6
kenziehh <ghaisani.nurani@gmail.com>
2024-05-22 20:41:41 +0700

    add highlight request

---
+++ app/Http/Requests/StoreHighlightRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreHighlightRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sport_match_id' => ['required', 'integer'],
+            'description' => ['required', 'string'],
+            'video_url' => ['required', 'string'],
+        ];
+    }
+}
