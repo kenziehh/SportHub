d8aa25caeec6d4e9142cc7fda0f80c5067d74889
kenziehh <ghaisani.nurani@gmail.com>
2024-05-31 01:36:49 +0700

    fix news layouting

--- resources/js/Pages/Home/Partials/NewsSection.tsx
+++ resources/js/Pages/Home/Partials/NewsSection.tsx
@@ -9,7 +9,7 @@ const NewsHome = ({ news, meta }: { news: NewsResponse[]; meta: Meta }) => {
     return (
         <>
             <section className="py-10 flex gap-6 md:gap-0 justify-between">
-                <h2 className="text-5xl font-semibold">News</h2>
+                <h2 className="text-5xl font-semibold">Hot News</h2>
                 <Link href="/news">
                     <PrimaryButton>See more</PrimaryButton>
                 </Link>
--- resources/js/Pages/News/Show.tsx
+++ resources/js/Pages/News/Show.tsx
@@ -18,7 +18,7 @@ export default function Show({ auth, newsData }: PageProps) {
                     className="max-h-[650px] w-full rounded-3xl"
                 />
                 <div className="absolute bottom-16 left-16 flex flex-col gap-y bg-transparent px-8 py-6 rounded-3xl">
-                    <h2 className="text-[24px] md:text-[56px] text-white">
+                    <h2 className="text-[24px] md:text-[56px] text-white font-semibold xl:max-w-[800px]">
                         {newsData.data.title}
                     </h2>
                     <span className="text-[#FFAB41]">
@@ -29,15 +29,7 @@ export default function Show({ auth, newsData }: PageProps) {
                 </div>
             </section>
             <section className="py-20">
-                {/* {newsData.data.content} */}
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-                quas ea rerum nesciunt illum quibusdam eum nam velit dignissimos
-                deserunt, quaerat debitis explicabo dolores eius magnam ab omnis
-                excepturi ullam? Lorem ipsum dolor, sit amet consectetur
-                adipisicing elit. Asperiores temporibus voluptate laudantium nam
-                magni consequatur necessitatibus. Impedit tempora unde quia vero
-                voluptatibus! Quam eaque exercitationem eveniet sapiente. Nisi,
-                asperiores magnam.
+                {newsData.data.content}
             </section>
         </Layout>
     );
